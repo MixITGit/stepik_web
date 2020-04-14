7ab4912d28bc1643687f3d4dd4e5f799f612b321
MixITGit <sidorov.ml99@gmail.com>
2020-04-14 21:03:11 +0000

    step 1 succ

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default.conft
 sudo /etc/init.d/nginx restart
 
+git config --global user.name "MixITGit"
+git config --global user.email "sidorov.ml99@gmail.com"
